25ca3e66751a5632ef084f93d70fab0e3303068c
AlehandroSV <alehandrosantos99@gmail.com>
2023-07-17 10:53:38 -0300

    Diferença de Strings

---
+++ ex004/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Diferença de Strings</title>
+</head>
+
+<body>
+    <h1>Diferença de Strings</h1>
+
+    <?php
+    $nome = 'Alehandro';
+
+    echo "Meu nome é $nome <br>";
+    echo 'Meu nome é $nome <br>';
+
+    const ESTADO = 'RS';
+
+    echo "Moro no ESTADO <br>";
+    echo 'Moro no ESTADO <br>';
+    echo 'Moro no ' . ESTADO;
+
+    ?>
+
+</body>
+
+</html>
